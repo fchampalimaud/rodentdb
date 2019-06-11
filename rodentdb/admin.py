5cbdccf703b8d3a6d048c09185a12e339c77fb76
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-06-11 13:31:43 +0100

    Update admin

--- rodentdb/admin.py
+++ rodentdb/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Rodent
+from . import models
 
 
-@admin.register(Rodent)
+@admin.register(models.Rodent)
 class RodentAdmin(admin.ModelAdmin):
     readonly_fields = ["created", "modified"]
+
+
+admin.site.register(models.Background)
+admin.site.register(models.Category)
+admin.site.register(models.Genotype)
+admin.site.register(models.Species)
