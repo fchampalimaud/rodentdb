d71f9867158029ea0e7398aee8dbf8b384ccec0e
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-06-21 14:21:18 +0100

    Make zygosity field optional

--- rodentdb/models/rodent.py
+++ rodentdb/models/rodent.py
@@ -32,7 +32,7 @@ class AbstractRodent(models.Model):
     origin = models.CharField(max_length=20)
     category = models.ForeignKey(to='rodentdb.Category', on_delete=models.PROTECT, related_name='rodents')
     background = models.ForeignKey(to='rodentdb.Background', on_delete=models.PROTECT, related_name='rodents', null=True, blank=True)
-    zygosity = models.ForeignKey(to='rodentdb.Zygosity', on_delete=models.PROTECT, related_name='rodents')
+    zygosity = models.ForeignKey(to='rodentdb.Zygosity', on_delete=models.PROTECT, related_name='rodents', null=True, blank=True)
     line_description = models.TextField(blank=True)
 
     class Meta:
