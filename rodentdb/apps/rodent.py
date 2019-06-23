9da278c9c56b20d8517db014324ce5eb3cce03ec
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-06-24 00:51:50 +0100

    Fix lookup

--- rodentdb/apps/rodent.py
+++ rodentdb/apps/rodent.py
@@ -132,7 +132,7 @@ class RodentApp(ModelAdminWidget):
             return True
 
         if user.memberships.filter(
-                group__accesses__db=cls.MODEL._meta.app_label
+                group__accesses__animaldb=cls.MODEL._meta.app_label
         ).exists():
             return True
 
