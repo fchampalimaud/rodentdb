817a7bb0392ac679a87a7da5d923200f707f9842
Luís Teixeira <micboucinha@gmail.com>
2019-12-02 16:52:04 +0000

    Add RodentResource to RodentAdmin

--- rodentdb/admin.py
+++ rodentdb/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
+from import_export import resources, widgets
+from import_export.admin import ExportActionMixin, ImportMixin
+from import_export.fields import Field
+from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 
 from . import models
+from .models import Rodent, Category, Species, Background, CoatColor, InducibleCassette, Origin, ReporterGene, Zygosity
 
+class RodentResource(resources.ModelResource):
+    background = Field(attribute='background', column_name='background', widget=ForeignKeyWidget(Background, 'name'))
+    category = Field(attribute='category', column_name='category', widget=ForeignKeyWidget(Category, 'name'))
+    coat_color = Field(attribute='coat_color', column_name='coat_color', widget=ForeignKeyWidget(CoatColor, 'name'))
+    inducible_cassette = Field(attribute='inducible_cassette', column_name='inducible_cassette', widget=ForeignKeyWidget(InducibleCassette, 'name'))
+    origin = Field(attribute='origin', column_name='origin', widget=ForeignKeyWidget(Origin, 'name'))
+    reporter_gene = Field(attribute='reporter_gene', column_name='reporter_gene', widget=ForeignKeyWidget(ReporterGene, 'name'))
+    species = Field(attribute='species', column_name='species', widget=ForeignKeyWidget(Species, 'name'))
+    zygosity = Field(attribute='zygosity', column_name='zygosity', widget=ManyToManyWidget(Zygosity, field='name'))
+    #TODO maintainer and ownership FKs missing
+    class Meta:
+        model = Rodent
+        skip_unchanged = True
+        clean_model_instances = True
 
 @admin.register(models.Rodent)
-class RodentAdmin(admin.ModelAdmin):
+class RodentAdmin(ImportMixin, ExportActionMixin, admin.ModelAdmin):
+    resource_class = RodentResource
     readonly_fields = ["created", "modified"]
 
 
