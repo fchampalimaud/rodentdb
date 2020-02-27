31845605f3ef5d5e0f882c080258dda4afbb93ad
Luís Teixeira <micboucinha@gmail.com>
2020-02-27 15:31:07 +0000

    Update maintainer to get user from get_user_model

--- rodentdb/admin.py
+++ rodentdb/admin.py
@@ -8,6 +8,7 @@ from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 from . import models
 from .models import Rodent, Category, Species, Background, CoatColor, InducibleCassette, Origin, ReporterGene, Zygosity
 from users.models import Group
+from django.contrib.auth import get_user_model
 
 class RodentResource(resources.ModelResource):
     background = Field(attribute='background', column_name='background', widget=ForeignKeyWidget(Background, 'name'))
@@ -18,7 +19,7 @@ class RodentResource(resources.ModelResource):
     reporter_gene = Field(attribute='reporter_gene', column_name='reporter_gene', widget=ForeignKeyWidget(ReporterGene, 'name'))
     species = Field(attribute='species', column_name='species', widget=ForeignKeyWidget(Species, 'name'))
     zygosity = Field(attribute='zygosity', column_name='zygosity', widget=ManyToManyWidget(Zygosity, field='name'))
-    maintainer = Field(attribute='maintainer', column_name='maintainer', widget=ForeignKeyWidget(settings.AUTH_USER_MODEL, 'name'))
+    maintainer = Field(attribute='maintainer', column_name='maintainer', widget=ForeignKeyWidget(get_user_model(), 'name'))
     ownership = Field(attribute='ownership', column_name='ownership', widget=ForeignKeyWidget(Group, 'name'))
 
     class Meta:
