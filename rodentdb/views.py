652b7712a5b6254d2491139d53e734726d483c0e
Luís Teixeira <micboucinha@gmail.com>
2020-03-10 17:11:17 +0000

    Update Rodent template generation to not depend on django-import-export

--- rodentdb/views.py
+++ rodentdb/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from rodentdb.admin import RodentResource
+from .models import Rodent
 import csv
 from django.http import HttpResponse
 
 # Create your views here.
 @login_required
 def get_rodent_template(request):
-    rodent_resource = RodentResource()
-    dataset = rodent_resource.export()
-    
+    fields = [item.name for item in Rodent()._meta.get_fields()]
+
+    ignore_export = ['_state', 'id', 'created', 'modified']
+    header_list = ['species', 'category', 'strain_name', 'common_name', 'background', 'zygosity', 'reporter_gene', 
+                   'inducible_cassette', 'coat_color', 'origin', 'origin_other', 
+                   'availability', 'link', 'mta', 'public', 'line_description', 'comments',
+                   'maintainer', 'ownership']
+
+    # add those fields that are not in either list to the end of the header_list
+    for item in fields:
+        if item not in ignore_export and item not in header_list:
+            header_list.append(item)
+
     # prepare response
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="rodent_template.csv"'
 
-    if dataset.csv:
-        header = dataset.csv.splitlines()
-        if len(header) > 0:
-            csv_list = header[0].split(',')
-            writer = csv.writer(response)
-            writer.writerow(csv_list)
+    writer = csv.writer(response)
+    writer.writerow(header_list)
 
     return response
